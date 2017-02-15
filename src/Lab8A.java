87a069b6012a7f7ff45338ac05a8b84e6ab263e8
Sharron J <sj7723@aol.com>
2017-02-15 10:44:50 -0500

    Demo Commit

--- src/Lab8A.java
+++ src/Lab8A.java
@@ -42,11 +42,9 @@ public class Lab8A {
         //Resources
         Scanner scan = new Scanner(System.in);
 
-        double batAve = 0.0; //variable for batting average
-        double slugPerc = 0.0; // slugging percentage variable
+
         int atBats = 0;             // variable for num of at bats that user inputs
-        int baseSum = 0;        // sum of all bases in the array input from user
-        int numbHits = 0;
+
         String answer = "";
         int numbBatters;
 
@@ -71,46 +69,72 @@ public class Lab8A {
             }
         }
 
+        //iterate through batters(rows)
+        for (int row = 0; row < batStats.length; row++) {
+            int bases = 0;
+            int hits = 0;
+            //iterate through this batters at-bats(columns)
+            for (int col = 0; col < batStats[row].length; col++) {
 
-        //user input determines length of array
-        int[] numbBats = new int[atBats];
-
-        //instruct user on what to input
-
-
-        //*request the number of bases
-        // * For each at bat, ask the  user for the number of bases earned by the batter
-        //traverse array and take input that assigns a number of bases earned per at-bat
-
-        for (int i = 0; i < numbBats.length; i++) {
-            //System.out.print("Enter the number of bases earned at the at-bat #" + (i + 1) + ": ");
-            /* use array to store at bats results for each player */
-            numbBats[i] = getAtBats(scan, "Enter the number of bases earned at the at-bat #" + (i + 1) + ": ");
-
-        }
-
-        //sum amount of bases in array indices
-        // counts number of hits
-        for (int i : numbBats) {
-            if (i > 0) {
-                numbHits++;
+                //add number of bases to total number
+                bases += batStats[row][col];
+                //if its a hit, increment the count of hits
+                if(batStats[row][col] > 0){
+                    hits++;
+                }
             }
-            baseSum += i;
+            //output the batting average for this batter
+            double batAvg = (double)hits / batStats[row].length;
+            System.out.printf("For batter %d, Batting Average: %.3f", (row + 1), batAvg);
+
+            //output the slugging percentage "        "
+            double slugPerc = (double)bases / batStats[row].length;
+            System.out.printf("\tSlugging Percentage: %.3f\n", slugPerc);
         }
 
-            /* Calculate the batting average and slugging percentage for one or more
-                baseball/softball players */
-        System.out.println("number of at-bats is: " + atBats);
-        System.out.println("sum of bases is: " + baseSum);
-        System.out.println("sum of hits is: " + numbHits);
 
-            /*After all the at-bats are entered, display the batting average and slugging
-            percentage of the batter */
-        System.out.printf("Batting average is: %.3f", (double) numbHits / atBats);
-        System.out.println("");
-        //printf may solve 3 decimal task %f
-        System.out.printf("Slugging percentage is: %.3f", (double) baseSum / atBats);
-        System.out.println("");
+
+
+
+//        //user input determines length of array
+//        int[] numbBats = new int[atBats];
+//
+//        //instruct user on what to input
+//
+//
+//        //*request the number of bases
+//        // * For each at bat, ask the  user for the number of bases earned by the batter
+//        //traverse array and take input that assigns a number of bases earned per at-bat
+//
+//        for (int i = 0; i < numbBats.length; i++) {
+//            //System.out.print("Enter the number of bases earned at the at-bat #" + (i + 1) + ": ");
+//            /* use array to store at bats results for each player */
+//            numbBats[i] = getAtBats(scan, "Enter the number of bases earned at the at-bat #" + (i + 1) + ": ");
+//
+//        }
+//
+//        //sum amount of bases in array indices
+//        // counts number of hits
+//        for (int i : numbBats) {
+//            if (i > 0) {
+//                numbHits++;
+//            }
+//            baseSum += i;
+//        }
+//
+//            /* Calculate the batting average and slugging percentage for one or more
+//                baseball/softball players */
+//        System.out.println("number of at-bats is: " + atBats);
+//        System.out.println("sum of bases is: " + baseSum);
+//        System.out.println("sum of hits is: " + numbHits);
+//
+//            /*After all the at-bats are entered, display the batting average and slugging
+//            percentage of the batter */
+//        System.out.printf("Batting average is: %.3f", (double) numbHits / atBats);
+//        System.out.println("");
+//        //printf may solve 3 decimal task %f
+//        System.out.printf("Slugging percentage is: %.3f", (double) baseSum / atBats);
+//        System.out.println("");
 
         System.out.println("Another batter? Y/N?");
         answer = scan.next();
